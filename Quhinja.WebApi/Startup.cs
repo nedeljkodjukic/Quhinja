cd6bdc0e3f326b310e51b5a4db56a3cd7d9ab4fa
5ardjordjevic <petardjordjevi.1@gmail.com>
2020-10-16 18:08:00 +0200

    Initial

--- Quhinja.WebApi/Startup.cs
+++ Quhinja.WebApi/Startup.cs
@@ -12,7 +12,7 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
 namespace Quhinja.WebApi
-{
+{//Nedeljko
     public class Startup
     {
         public Startup(IConfiguration configuration)
